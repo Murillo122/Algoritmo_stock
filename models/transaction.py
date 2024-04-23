467a83793b6197160611fbb55d97de90b174981d
Murillo122 <71354963+Murillo122@users.noreply.github.com>
2024-04-23 00:34:36 +0000

    feat editando csv por id

--- models/transaction.py
+++ models/transaction.py
@@ -1,15 +1,19 @@
 from utils.utils import Utils
 
 class Transaction():
-    def __init__(self, type = None, value = None, description = None):
-        self.__type = type
-        self.__value = value
-        self.__description = description
-        self.__utils = Utils()
+    def __init__(self, id=None, codigo=None, descricao=None, quantidade=None, utils=None):
+        self.__id = id
+        self.__codigo = codigo
+        self.__descricao = descricao
+        self.__quantidade = quantidade
+        self.__utils = utils if utils else Utils()
     
     def save(self):
-        self.__utils.write_file(self.__type, self.__value, self.__description)
+        self.__utils.write_file_csv(self.__id, self.__codigo, self.__descricao, self.__quantidade)
     
     def view(self):
-        for transaction in self.__utils.read_file():
-            print(transaction)
+        for transaction in self.__utils.read_file_csv():
+            print(f"id: {transaction['id']}, codigo: {transaction['codigo']}, descricao: {transaction['descricao']}, quantidade: {transaction['quantidade']}")
+
+    def update(self, id, new_codigo, new_descricao, new_quantidade):
+        self.__utils.update_transaction(id, new_codigo, new_descricao, new_quantidade)
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+import csv
 from configurations.configurations import Configurations
 from datetime import date
 
@@ -5,12 +6,38 @@ class Utils():
     def __init__(self):
         self.__configurations = Configurations()
 
-    def read_file(self):
-        with open(self.__configurations.file_output, 'r') as file:
-            return list(map(lambda l: l.replace('\n', '') , file.readlines()))
+    def read_file_csv(self):
+        transactions = []
+        with open(self.__configurations.file_output, mode='r', newline='') as file:
+            reader = csv.DictReader(file, delimiter=';')
+            for row in reader:
+                transactions.append(row)
+        return transactions
     
-    def write_file(self, _type, value, description):
-        with open(self.__configurations.file_output, 'a+') as file:
-            file.write(
-                f'\n{date.today()} - {_type} - {value} - {description}'
-            )
+    def write_file_csv(self, id, codigo, descricao, quantidade):
+        with open(self.__configurations.file_output, mode='a', newline='') as file:
+            writer = csv.writer(file, delimiter=';')
+            writer.writerow([id, codigo, descricao, quantidade])
+
+    def update_transaction(self, id, new_codigo, new_descricao, new_quantidade):
+        transactions = self.read_file_csv()
+        found = False
+        for transaction in transactions:
+            if transaction['id'] == str(id):
+                transaction['codigo'] = new_codigo
+                transaction['descricao'] = new_descricao
+                transaction['quantidade'] = new_quantidade
+                found = True
+                break
+        
+        if not found:
+            print("ID inválido. Não foi possível encontrar a transação com o ID fornecido.")
+            return
+
+        with open(self.__configurations.file_output, mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'codigo', 'descricao', 'quantidade'], delimiter=';')
+            writer.writeheader()
+            for transaction in transactions:
+                writer.writerow(transaction)
+
+
